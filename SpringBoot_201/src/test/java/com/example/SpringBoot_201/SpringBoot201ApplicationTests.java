b3e41035ad19b963927f7d7a3e6896687a244b8e
Domonique Nelthrope <dnelthr@wgu.edu>
2023-09-20 19:04:31 -0400

    Handling form values the hard way
    Capturing form values in the controller
    Using the thymeleaf th:action attribute

---
+++ SpringBoot_201/src/test/java/com/example/SpringBoot_201/SpringBoot201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
