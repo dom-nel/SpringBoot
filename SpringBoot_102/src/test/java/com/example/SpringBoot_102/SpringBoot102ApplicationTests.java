dcfdb63ac68ba5f20d22ee169e5b2a107c12b602
Domonique Nelthrope <dnelthr@wgu.edu>
2023-09-20 11:23:41 -0400

    Using  a variable on a template
    Passing a variable to the template from the controller
    
    Using the Thymeleaf th:text attribute

---
+++ SpringBoot_102/src/test/java/com/example/SpringBoot_102/SpringBoot102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
