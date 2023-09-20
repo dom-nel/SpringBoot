aea0c93722867dfd01c1990423b657bb404af42d
Domonique Nelthrope <dnelthr@wgu.edu>
2023-09-20 17:59:09 -0400

    Using a property variable on a template
    Loading a value in the template from the messages.properties file

---
+++ SpringBoot_103/src/main/java/com/example/SpringBoot_103/SpringBoot103Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot103Application.class, args);
+	}
+
+}
---
+++ SpringBoot_103/src/test/java/com/example/SpringBoot_103/SpringBoot103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot103ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
