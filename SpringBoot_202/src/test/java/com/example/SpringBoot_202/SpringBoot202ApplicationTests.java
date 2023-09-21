cc177060169aa122ec8b94f76d46435c342f2e4f
Domonique Nelthrope <dnelthr@wgu.edu>
2023-09-21 12:17:25 -0400

    Handling From Values The easy way
    Retrieving values from a form using a Java Bean
    
    Using the thymeleaf th:object attribute
    
    Using the thymeleaf th:field attribute

---
+++ SpringBoot_202/src/test/java/com/example/SpringBoot_202/SpringBoot202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
